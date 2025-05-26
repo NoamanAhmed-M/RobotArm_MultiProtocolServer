adf67945f5480871e6c1c968840b0c99efd8043c
NoamanAhmed-M <77328177+NoamanAhmed-M@users.noreply.github.com>
2025-05-26 11:59:56 +0200

    Update yolo_seg.py

--- AI/yolo_seg.py
+++ AI/yolo_seg.py
@@ -138,3 +138,5 @@ source ~/.bashrc
 trtexec --onnx=best_nan_sego.onnx --saveEngine="best_nan_sego.engine" --fp32
 trtexec --onnx=best_nan_sego.onnx --saveEngine=best_nan_sego.engine --fp32
 --saveEngine=/home/its/Desktop/best_nan_sego_fp32.engine
+trtexec --onnx=/home/its/Desktop/yolov5/best_nan_sego.onnx --saveEngine=/home/its/Desktop/yolov5/best_nan_sego_fp32.engine --fp32 --verbose
+
