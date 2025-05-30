26a86cabc1026f0807ef9c71a42d05f3ef06a7a7
NoamanAhmed-M <77328177+NoamanAhmed-M@users.noreply.github.com>
2025-05-30 20:25:56 +0200

    Create pip33tst.py

---
+++ rover/pip33tst.py
@@ -0,0 +1,13 @@
+import Jetson.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(19, GPIO.OUT)
+
+print("Turning GPIO19 ON")
+GPIO.output(19, GPIO.HIGH)
+time.sleep(3)
+
+print("Turning GPIO19 OFF")
+GPIO.output(19, GPIO.LOW)
+GPIO.cleanup()
