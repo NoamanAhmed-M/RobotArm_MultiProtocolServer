0fdf88c6839c16aaf38cd113439531579b0e90e5
NoamanAhmed-M <77328177+NoamanAhmed-M@users.noreply.github.com>
2025-05-30 20:04:43 +0200

    Update tst2.py

--- rover/tst2.py
+++ rover/tst2.py
@@ -1,47 +1,14 @@
-#!/usr/bin/env python3
-
 import Jetson.GPIO as GPIO
 import time
 
-# BCM GPIO numbers
-ENA = 18   # Pin 32
-IN1 = 25   # Pin 22
-IN2 = 27   # Pin 13
-
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
 
-# Setup GPIO pins
-GPIO.setup(ENA, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(IN1, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(IN2, GPIO.OUT, initial=GPIO.LOW)
+print("Toggling Pin 32 (GPIO18)...")
+for _ in range(10):
+    GPIO.output(18, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(18, GPIO.LOW)
+    time.sleep(0.5)
 
-try:
-    print("Motor STOP")
-    GPIO.output(ENA, GPIO.HIGH)
-    GPIO.output(IN1, GPIO.LOW)
-    GPIO.output(IN2, GPIO.LOW)
-    time.sleep(1)
-
-    print("Motor FORWARD")
-    GPIO.output(IN1, GPIO.HIGH)
-    GPIO.output(IN2, GPIO.LOW)
-    time.sleep(1)
-
-    print("Motor STOP")
-    GPIO.output(IN1, GPIO.LOW)
-    GPIO.output(IN2, GPIO.LOW)
-    time.sleep(1)
-
-    print("Motor BACKWARD")
-    GPIO.output(IN1, GPIO.LOW)
-    GPIO.output(IN2, GPIO.HIGH)
-    time.sleep(1)
-
-    print("Final STOP")
-    GPIO.output(ENA, GPIO.LOW)
-    GPIO.output(IN1, GPIO.LOW)
-    GPIO.output(IN2, GPIO.LOW)
-    time.sleep(1)
-
-finally:
-    GPIO.cleanup()
+GPIO.cleanup()
