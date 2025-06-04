7ce50fac28fdcad0fa180f1addbac9cecf7c1c08
NoamanAhmed-M <77328177+NoamanAhmed-M@users.noreply.github.com>
2025-06-04 15:31:24 +0200

    Update tst.py

--- AI/tst/tst.py
+++ AI/tst/tst.py
@@ -3,3 +3,10 @@ SUBSYSTEM=="gpio*", PROGRAM="/bin/sh -c 'chown -R root:gpio /sys/class/gpio/ && 
 sudo udevadm control --reload-rules
 sudo udevadm trigger
 sudo reboot
+---------------
+import Jetson.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+GPIO.output(18, GPIO.HIGH)
+GPIO.cleanup()
