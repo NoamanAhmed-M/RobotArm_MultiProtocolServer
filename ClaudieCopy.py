5a7e1ac431843d57d7d888a65c12bcd4630eeffa
Muhammad ElGendy <77328102+MuhammadElGendys@users.noreply.github.com>
2025-06-08 01:03:34 +0200

    Update ClaudieCopy.py

--- ClaudieCopy.py
+++ ClaudieCopy.py
@@ -1,1 +1,6 @@
+#turn on
 #1:00:56 AM.101 INFO Chat message: {"type":"status","msg":"Command received: {'type': 'command', 'value': True}","timestamp":1749337255.8241785}
+
+
+#turn off
+#1:03:04 AM.804 INFO Chat message: {"type":"status","msg":"Command received: {'type': 'command', 'value': False}","timestamp":1749337384.501489}
